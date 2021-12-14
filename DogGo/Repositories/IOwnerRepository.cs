76799edb4d9dca7e4d9e4ad9d3c529b844c9cce2
scrawleyy1 <crawley.sydney@gmail.com>
2021-12-14 09:15:14 -0800

    delete and edit

--- DogGo/Repositories/IOwnerRepository.cs
+++ DogGo/Repositories/IOwnerRepository.cs
@@ -8,8 +8,11 @@ namespace DogGo.Repositories
 {
     public interface IOwnerRepository
     {
+        void AddOwner(Owner owner);
+        void DeleteOwner(int ownerId);
         List<Owner> GetAllOwners();
-
+        Owner GetOwnerByEmail(string email);
         Owner GetOwnerById(int id);
+        void UpdateOwner(Owner owner);
     }
 }
